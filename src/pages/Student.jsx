f20e951e1fcde9b6349564937b69fd7b855dcba5
seoAreum <1971236@hansung.ac.kr>
2024-02-05 21:26:16 +0900

    init: page 파일 초기 세팅

---
+++ src/pages/Student.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PageLayout from "../components/Student/PageLayout";
+
+const Student = () => {
+  return (
+    <PageLayout>
+      <div>StudentPage</div>
+    </PageLayout>
+  );
+};
+
+export default Student;
