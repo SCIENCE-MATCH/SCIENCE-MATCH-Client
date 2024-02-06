9692d13ec750a2f8d8dab90789f0e91f51c5bdc9
seoAreum <1971236@hansung.ac.kr>
2024-02-06 13:21:00 +0900

    style: '내 정보' 버튼 조건부 스타일링

--- src/components/Student/StudentHeader.jsx
+++ src/components/Student/StudentHeader.jsx
@@ -1,22 +1,16 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
-import styled from "styled-components";
-import { NAV_LIST } from "../../pages/Student"; 
+import styled, { css } from "styled-components";
+import { HEADER_DATA } from "../../pages/Student";
 
 const StudentHeader = ({ clickedList, handleClickList }) => {
-  const navigate = useNavigate();
-
-  const handleClickInfoBtn = () => {
-    // 추후 경로 확정 시, 수정 예정!
-    navigate("/");
-  };
+  const lastData = HEADER_DATA.length - 1;
 
   return (
     <St.HeaderWrapper>
       <St.Title>Science Match</St.Title>
 
       <St.NavBar>
-        {NAV_LIST.map((it) => {
+        {HEADER_DATA.slice(0, lastData).map((it) => {
           return (
             <St.List key={it} $isClickedList={it === clickedList} onClick={(e) => handleClickList(e.target.innerHTML)}>
               {it}
@@ -26,8 +20,12 @@ const StudentHeader = ({ clickedList, handleClickList }) => {
       </St.NavBar>
 
       <St.InfoBtnWrapper>
-        <St.InfoBtn type="button" onClick={handleClickInfoBtn}>
-          내 정보
+        <St.InfoBtn
+          type="button"
+          $isClickedList={HEADER_DATA[lastData] === clickedList}
+          onClick={(e) => handleClickList(e.target.innerHTML)}
+        >
+          {HEADER_DATA[lastData]}
         </St.InfoBtn>
       </St.InfoBtnWrapper>
     </St.HeaderWrapper>
@@ -81,9 +79,17 @@ const St = {
     padding: 0.7rem 2rem;
 
     border-radius: 3rem;
-    border: 0.05rem solid #000000;
+    ${({ $isClickedList, theme }) =>
+      $isClickedList
+        ? css`
+            border: 0.05rem solid ${theme.colors.mainColor};
+            color: ${theme.colors.mainColor};
+          `
+        : css`
+            border: 0.05rem solid #000000;
+            color: ${theme.colors.infoBtn};
+          `};
 
-    color: ${({ theme }) => theme.colors.infoBtn};
     line-height: 1.936rem;
     font-weight: 700;
     font-size: 1.6rem;
