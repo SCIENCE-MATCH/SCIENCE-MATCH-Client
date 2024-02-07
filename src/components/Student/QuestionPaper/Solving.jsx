8235696b3bd977ee138219a0e958ddbd73abbb6e
seoAreum <1971236@hansung.ac.kr>
2024-02-08 07:04:42 +0900

    feat: 학습지 상세 뷰1차 퍼블리싱

--- src/components/Student/QuestionPaper/Solving.jsx
+++ src/components/Student/QuestionPaper/Solving.jsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
+import PageLayout from "./PageLayout";
 
-const Solving = () => {
-    return (
-        <div>
-            solving
-        </div>
-    );
+const Solving = ({ handleClickedCloseBtn }) => {
+  return (
+    <PageLayout handleClickedCloseBtn={handleClickedCloseBtn}>
+      <St.QuestionWrapper>
+        {/* 추후 학습지 이미지로 대체할 예정 */}
+        <St.QuestionImg></St.QuestionImg>
+      </St.QuestionWrapper>
+    </PageLayout>
+  );
 };
 
 export default Solving;
+
+const St = {
+  QuestionWrapper: styled.div`
+    width: 100%;
+    height: 65rem;
+
+    padding: 2.8rem 5.8rem;
+
+    border-radius: 1.5rem;
+
+    background-color: ${({ theme }) => theme.colors.brightGray};
+  `,
+
+  QuestionImg: styled.div`
+    width: 100%;
+    height: 100%;
+  `,
+};
