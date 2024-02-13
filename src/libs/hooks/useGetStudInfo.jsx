eb7002054f1fbadb2bd1d6216ddd210fe8f7cb03
seoAreum <1971236@hansung.ac.kr>
2024-02-14 02:18:01 +0900

    feat: 학생 정보 불러오는 훅 생성

---
+++ src/libs/hooks/useGetStudInfo.jsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+import { api } from "../api";
+
+const useGetStudInfo = () => {
+  const [data, setData] = useState();
+
+  const fetchData = async () => {
+    await api
+      .get("/student/mypage")
+      .then((res) => setData(res.data.data))
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return { data };
+};
+
+export default useGetStudInfo;
