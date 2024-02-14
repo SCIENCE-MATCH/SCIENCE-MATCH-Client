42a81867962094e0b07aaffd85a53ba4269bb5e8
seoAreum <1971236@hansung.ac.kr>
2024-02-15 04:43:13 +0900

    feat: cookie 활용 코드

--- src/libs/api.js
+++ src/libs/api.js
@@ -1,9 +1,12 @@
 import axios from "axios";
+import { getCookie } from "../components/_Common/cookie";
+
+const accessToken = getCookie("aToken");
 
 export const api = axios.create({
   baseURL: process.env.REACT_APP_BASE_URL,
   headers: {
-    Authorization: `Bearer ${process.env.REACT_APP_DUMMY_TOKEN}`,
+    Authorization: accessToken && `Bearer ${accessToken}`,
     "Content-Type": "application/json",
   },
 });
