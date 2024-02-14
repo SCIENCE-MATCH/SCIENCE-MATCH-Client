316cd19221b3bcf099f7d034700d5780867a021c
seoAreum <1971236@hansung.ac.kr>
2024-02-14 19:31:52 +0900

    feat: 학습지 제출 훅

---
+++ src/libs/apis/postQuestionPaper.js
@@ -0,0 +1,22 @@
+import { api } from "../api";
+
+const postQuestionPaper = async ({ input, id, questionNum, handleClickedCloseBtn }) => {
+  const answer = input && input.map((data) => data.answer);
+  if (answer && answer.length === parseInt(questionNum)) {
+    await api
+      .post("/student/question-paper", {
+        assignQuestionPaperId: id,
+        answer: answer,
+      })
+      .then((res) => {
+        // 데이터 들어오면 확인용으로 남겨둠 -> 확인한 뒤에 지울 예정
+        console.log(res);
+        handleClickedCloseBtn();
+      })
+      .catch((err) => console.log(err));
+  } else {
+    alert("답을 모두 입력해주세요.");
+  }
+};
+
+export default postQuestionPaper;
